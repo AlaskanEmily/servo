bc4c809393ceef66c2f562547b9d29a668eadaf0
Josh Matthews <josh@joshmatthews.net>
2021-02-19 10:15:43 -0500

    fix media stack check

--- python/servo/build_commands.py
+++ python/servo/build_commands.py
@@ -195,6 +195,7 @@ class MachCommands(CommandBase):
 
         media_stack = self.pick_media_stack(media_stack, target)
         features += media_stack
+        has_media_stack = media_stack != ["media-dummy"]
 
         target_path = base_path = self.get_target_dir()
         if android:
@@ -727,7 +728,7 @@ class MachCommands(CommandBase):
                         status = 1
 
                 # copy needed gstreamer DLLs in to servo.exe dir
-                if media_stack != "media-dummy":
+                if has_media_stack:
                     print("Packaging gstreamer DLLs")
                     if not package_gstreamer_dlls(env, servo_exe_dir, target_triple, uwp):
                         status = 1
@@ -743,7 +744,7 @@ class MachCommands(CommandBase):
                 )
                 assert os.path.exists(servo_exe_dir)
 
-                if media_stack != "media-dummy" and not package_gstreamer_dylibs(servo_exe_dir):
+                if has_media_stack and not package_gstreamer_dylibs(servo_exe_dir):
                     return 1
 
                 # On the Mac, set a lovely icon. This makes it easier to pick out the Servo binary in tools
